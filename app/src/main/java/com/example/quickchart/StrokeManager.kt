40b4771a7375a029b051d055b0b25fd6067e9199
getekj <joannagetek@gmail.com>
2023-09-12 20:47:39 -0400

    Added StrokeManager which builds the Ink object, downloads the DigitalInkRecognizer en-US model, and processes the Ink object

---
+++ app/src/main/java/com/example/quickchart/StrokeManager.kt
@@ -0,0 +1,143 @@
+package com.example.quickchart
+
+
+// adapted from: https://developers.google.com/ml-kit/vision/digital-ink-recognition/android
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import android.view.MotionEvent
+import android.widget.TextView
+import com.google.mlkit.common.MlKitException
+import com.google.mlkit.common.model.DownloadConditions
+import com.google.mlkit.common.model.RemoteModelManager
+import com.google.mlkit.vision.digitalink.DigitalInkRecognition
+import com.google.mlkit.vision.digitalink.DigitalInkRecognitionModel
+import com.google.mlkit.vision.digitalink.DigitalInkRecognitionModelIdentifier
+import com.google.mlkit.vision.digitalink.DigitalInkRecognizer
+import com.google.mlkit.vision.digitalink.DigitalInkRecognizerOptions
+import com.google.mlkit.vision.digitalink.Ink
+
+object StrokeManager {
+
+    // holds the digital ink recognition model, it is currently init to null
+    private val model: DigitalInkRecognitionModel?= null
+
+    // holds the Ink Object which is drawn from the touch screen at the addNewTouchEvent
+    private var inkBuilder = Ink.builder()
+
+    // builds the stokes (pen/stylus movements) within the Ink object
+    private var strokeBuilder = Ink.Stroke.builder()
+
+    // call this function each time there is a new event
+    fun addNewTouchEvent(event:MotionEvent) {
+
+        // returns the masked action being performed
+        val action = event.actionMasked
+        val x = event.x
+        val y = event.y
+        // timing information (may use to measure performance later but can be omitted with ink point create)
+        val t = System.currentTimeMillis()
+
+
+        when (action) {
+
+            // a pressed gesture has started, the current action is the stylus/pen is down
+            // the motion contains the initial starting location
+            MotionEvent.ACTION_DOWN -> {
+                // creating a new Ink stroke and assigning to strokeBuilder
+                strokeBuilder = Ink.Stroke.builder()
+                // adding the starting point of the stroke to the (xy coords + timestamp) to the strokeBuilder
+                strokeBuilder!!.addPoint(Ink.Point.create(x,y,t))
+
+            }
+
+            // a change happened between press gesture (btwn ACTION_DOWN and ACTION_UP)
+            // the motion contains the most recent point and any intermediate pts since the last down
+            // or move event
+            MotionEvent.ACTION_MOVE -> strokeBuilder!!.addPoint(Ink.Point.create(x,y,t))
+
+            // a press gesture has finished, the motion contains the final release location as
+            // well as any intermediate points since the last down or move event
+            MotionEvent.ACTION_UP -> {
+                // adds the end point to the currently active stroke
+                strokeBuilder!!.addPoint(Ink.Point.create(x,y,t))
+                // now the stroke has been fully build on release, we add it to the ink object builder to
+                // assemble all the strokes into the overall inkBuilder (the user's ink input)
+                inkBuilder.addStroke(strokeBuilder!!.build())
+
+                // clearing the stroke builder
+                strokeBuilder = Ink.Stroke.builder()
+            }
+        }
+
+        // send ink to the recognizer?
+
+    }
+
+    fun downloadModel() {
+
+        // Specify the recognition model for a language and download so it is available locally
+        // modelIdentifier is set to null and will be used to store the identifier for the digital ink recognition model
+        var modelIdentifier: DigitalInkRecognitionModelIdentifier ?= null
+
+        try {
+            modelIdentifier = DigitalInkRecognitionModelIdentifier.fromLanguageTag("en-US")
+        } catch (e: MlKitException) {
+            // language tag failed to parse, handle error.
+            Log.i(TAG, "Failed to parse language tag")
+        }
+        if (modelIdentifier == null) {
+            // no model was found, handle error.
+            Log.i(TAG, "No model found")
+            return
+        }
+//        var model: DigitalInkRecognitionModel? =
+//            modelIdentifier?.let { DigitalInkRecognitionModel.builder(it).build() }
+
+        // creates the model from identifier
+        var model = DigitalInkRecognitionModel.builder(modelIdentifier!!).build()
+
+        val remoteModelManager = RemoteModelManager.getInstance()
+
+        remoteModelManager.download(model, DownloadConditions.Builder().build())
+            .addOnSuccessListener {
+                Log.i(TAG,"Model successfully downloaded")
+            }
+            .addOnFailureListener {
+                Log.i(TAG, "Error while downloading the model")
+            }
+
+
+    }
+
+    // pass in ink object instead?
+    fun recognize(textView: TextView) {
+
+        // Get a recognizer for the language
+        var recognizer: DigitalInkRecognizer =
+            DigitalInkRecognition.getClient(
+                DigitalInkRecognizerOptions.builder(model!!).build())
+
+        // Process Ink Object
+        val ink = inkBuilder.build()
+
+        recognizer.recognize(ink)
+            .addOnSuccessListener {
+
+                textView.text = it.candidates[0].text
+
+            }
+            .addOnFailureListener {
+
+                Log.i(TAG, "Error during recognition of ink object")
+
+            }
+
+    }
+
+    fun clear() {
+
+        inkBuilder = Ink.builder()
+
+    }
+}
--- app/src/main/java/com/example/quickchart/ui/CodeChartScreen.kt
+++ app/src/main/java/com/example/quickchart/ui/CodeChartScreen.kt
@@ -1,13 +1,17 @@
 package com.example.quickchart.ui
 
+
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 
 @Composable
 fun CodeChartScreen(patient: String, navController: NavController ) {
     Text(text = "this is code chart screen patient id: $patient")
+
 }
 
 
